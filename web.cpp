20c366f933ba5116c589b78d15aedcd9915b739e
sebaherrera74 <sebaherrera152@gmail.com>
2026-06-29 00:16:16 -0300

    feat: add dynamic web page with live sensor values

--- web.cpp
+++ web.cpp
@@ -1,6 +1,8 @@
 #include "Web.h"
 #include <WebServer.h>
 #include "log.h"
+#include "sensor.h"
+
 WebServer server(80);
 WebManager web;
 
@@ -10,8 +12,9 @@ void handleRoot()
     server.send(
         200,
         "text/html",
-        "<h1>SIEE ESP32 BASE</h1>");
+        web.buildPage());
 }
+
 void WebManager::begin()
 {
   server.on("/", handleRoot);
@@ -24,4 +27,47 @@ void WebManager::begin()
 void WebManager::update()
 {
   server.handleClient();
+}
+
+String WebManager::buildPage()
+{
+    String html;
+
+    html += "<html>";
+
+    html += "<head>";
+    html += "<title>SIEE ESP32 BASE</title>";
+    html += "<meta http-equiv=\"refresh\" content=\"2\">";
+    html += "</head>";
+  
+    html += "<body>";
+
+    html += "<h1>SIEE ESP32 BASE</h1>";
+
+    html += "<hr>";
+
+    html += "<h2>CO2</h2>";
+    html += "<p>";
+    html += String(sensor.getCO2(),0);
+    html += " ppm</p>";
+
+    html += "<h2>Temperatura</h2>";
+    html += "<p>";
+    html += String(sensor.getTemperature(),1);
+    html += " C</p>";
+
+    html += "<h2>Humedad</h2>";
+    html += "<p>";
+    html += String(sensor.getHumidity(),1);
+    html += " %</p>";
+
+    html += "<hr>";
+
+    html += "<p>SIEE Framework</p>";
+
+    html += "</body>";
+
+    html += "</html>";
+
+    return html;
 }
--- web.h
+++ web.h
@@ -10,6 +10,7 @@ public:
     void begin();
 
     void update();
+    String buildPage();
 
 };
 
